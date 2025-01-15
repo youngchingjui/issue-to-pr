5362d32bd6d1f2964021b2bb6cf1bc0a4358874c
Ching Jui Young <young.chingjui@gmail.com>
2025-01-15 14:37:14 +0800

    Removed dropdown menu; only display 'Fix Issue & Create PR' button on issues page.

--- components/IssueActionsDropdown.tsx
+++ components/IssueActionsDropdown.tsx
@@ -1,15 +1,6 @@
 "use client"
 
-import { MoreHorizontalIcon } from "lucide-react"
-
 import { CreatePullRequestButton } from "@/components/CreatePullRequestButton"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { GitHubRepository } from "@/lib/types"
 
 export function IssueActionsDropdown({
@@ -19,35 +10,7 @@ export function IssueActionsDropdown({
   issueNumber: number
   repo: GitHubRepository
 }) {
-  const handleCommitCode = async (issueNumber: number) => {
-    // Implement commit code logic, likely calling an API route
-    console.log(`Committing code for issue ${issueNumber}`)
-    // Similar API call as above
-  }
-
-  const handleGitPush = async (issueNumber: number) => {
-    // Implement git push logic, likely calling an API route
-    console.log(`Pushing code for issue ${issueNumber}`)
-    // Similar API call as above
-  }
-
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="flex items-center justify-center w-8 h-8">
-        <MoreHorizontalIcon className="w-4 h-4" />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        <DropdownMenuItem onClick={() => handleCommitCode(issueNumber)}>
-          Commit Code
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => handleGitPush(issueNumber)}>
-          Git Push
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <CreatePullRequestButton issueNumber={issueNumber} repo={repo} />
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <CreatePullRequestButton issueNumber={issueNumber} repo={repo} />
   )
 }
