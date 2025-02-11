b103c0978ce3b12d1e9b2105f4821159257b00a5
Ching Jui Young <young.chingjui@gmail.com>
2025-02-11 13:31:30 +0800

    Automatically link PRs to issues by appending closing keyword

--- lib/github/pullRequests.ts
+++ lib/github/pullRequests.ts
@@ -29,20 +29,27 @@ export async function createPullRequest({
   branch,
   title,
   body,
+  issueNumber,
 }: {
   repo: string
   branch: string
   title: string
   body: string
+  issueNumber?: number
 }) {
   const octokit = await getOctokit()
   const user = await getGithubUser()
+
+  let prBody = body;
+  if (issueNumber) {
+    prBody += `\n\nCloses #${issueNumber}`;
+  }
 
   const pullRequest = await octokit.pulls.create({
     owner: user.login,
     repo,
     title,
-    body,
+    body: prBody,
     head: branch,
     base: "main",
   })
