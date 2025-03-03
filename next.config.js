bdea5501cc9636f6fcd15a4bf9ba970d4f46a13d
Ching Jui Young <young.chingjui@gmail.com>
2025-03-03 13:13:30 +0800

    fixed route

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/blog",
+        source: "/blogs",
         headers: [
           {
             key: "Content-Security-Policy",
