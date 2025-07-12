5c68a4abc510d05505e3daca884078246bbdc53c
Ching Jui Young <young.chingjui@gmail.com>
2025-07-12 10:55:37 +0800

    prettier

--- components/workflow-runs/events/LLMResponseEvent.tsx
+++ components/workflow-runs/events/LLMResponseEvent.tsx
@@ -10,8 +10,8 @@ import { PostToGitHubButton } from "@/components/issues/actions/PostToGitHubButt
 import { ResolveIssueButton } from "@/components/issues/actions/ResolveIssueButton"
 import { Button } from "@/components/ui/button"
 import { CollapsibleContent } from "@/components/ui/collapsible-content"
-import { EventTime } from "@/components/workflow-runs/events/EventTime"
 import { CopyMarkdownButton } from "@/components/workflow-runs/events/CopyMarkdownButton"
+import { EventTime } from "@/components/workflow-runs/events/EventTime"
 import { Issue, LLMResponse, LLMResponseWithPlan } from "@/lib/types"
 
 // Some LLM response event nodes will also be a Plan node
