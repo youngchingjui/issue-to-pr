e0126b4aa137a1171bd05d304eb9cfae350e095d
Ching Jui Young <young.chingjui@gmail.com>
2025-08-07 18:17:26 +0800

    removed non-working eslint rules

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,25 +1,25 @@
 // Core ports
+export type { RedisPort } from "./core/ports/RedisPort"
 export type {
-  WorkerPort,
-  WorkerJobData,
-  WorkerJobResult,
   WorkerEvent,
   WorkerEventType,
+  WorkerJobData,
+  WorkerJobResult,
+  WorkerPort,
 } from "./core/ports/WorkerPort"
-export type { RedisPort } from "./core/ports/RedisPort"
 
 // Business logic services
+export { createRedisService, RedisService } from "./lib/redis"
+export type { QueueDefinition, WorkerDefinition } from "./lib/WorkerService"
 export { WorkerService } from "./lib/WorkerService"
-export type { WorkerDefinition, QueueDefinition } from "./lib/WorkerService"
-export { RedisService, createRedisService } from "./lib/redis"
 
 // Job processors
-export { processAutoResolveIssue } from "./lib/jobs/autoResolveIssue"
-export { processCommentOnIssue } from "./lib/jobs/commentOnIssue"
-export { processResolveIssue } from "./lib/jobs/resolveIssue"
 export type { AutoResolveIssueJobData } from "./lib/jobs/autoResolveIssue"
+export { processAutoResolveIssue } from "./lib/jobs/autoResolveIssue"
 export type { CommentOnIssueJobData } from "./lib/jobs/commentOnIssue"
+export { processCommentOnIssue } from "./lib/jobs/commentOnIssue"
 export type { ResolveIssueJobData } from "./lib/jobs/resolveIssue"
+export { processResolveIssue } from "./lib/jobs/resolveIssue"
 
 // Infrastructure adapters
 export { BullMQAdapter } from "./adapters/BullMQAdapter"
