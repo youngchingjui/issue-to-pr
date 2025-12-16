d13cd102d928682f2da6d6f9964647ea5d8b4734
Ching Jui Young <young.chingjui@gmail.com>
2025-12-16 09:48:34 +0800

    fixes @ alias

--- shared/src/ui/alert.tsx
+++ shared/src/ui/alert.tsx
@@ -1,7 +1,6 @@
+import { cn } from "@shared/ui/cn"
 import { cva, type VariantProps } from "class-variance-authority"
 import * as React from "react"
-
-import { cn } from "@/ui/cn"
 
 const alertVariants = cva(
   "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
