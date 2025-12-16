1413a9d41667dcaffba99997fa60d01d8d9505db
Ching Jui Young <young.chingjui@gmail.com>
2025-12-16 22:28:26 +0800

    add check

--- shared/src/adapters/github/octokit/graphql/pullRequest.reader.ts
+++ shared/src/adapters/github/octokit/graphql/pullRequest.reader.ts
@@ -281,6 +281,9 @@ export const getPullRequestMetaAndLinkedIssue = async (
   auth: { authProvider: GitHubAuthProvider; authTarget: GitHubAuthTarget }
 ) => {
   const [owner, repo] = repoFullName.split("/")
+  if (!owner || !repo) {
+    throw new Error("Invalid repoFullName. Expected 'owner/repo'")
+  }
   const { authProvider, authTarget } = auth
 
   const { graphql } = await authProvider.getClient(authTarget)
