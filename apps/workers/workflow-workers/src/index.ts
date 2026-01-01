2282ff1de6c19c86712ec5d904cca4536c7202e6
Ching Jui Young <young.chingjui@gmail.com>
2026-01-01 23:55:53 +0800

    migrate to shared

--- apps/workers/workflow-workers/src/index.ts
+++ apps/workers/workflow-workers/src/index.ts
@@ -11,8 +11,8 @@
 
  */
 import { QueueEvents, Worker } from "bullmq"
-import { getRedisConnection } from "shared/adapters/ioredis/client"
 
+import { getRedisConnection } from "@/shared/adapters/ioredis/client"
 import { WORKFLOW_JOBS_QUEUE } from "@/shared/entities/Queue"
 
 import { handler } from "./handler"
