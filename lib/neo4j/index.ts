a64adce2e5ec0593f417d06d0238544e3e51c1ba
Ching Jui Young <young.chingjui@gmail.com>
2026-01-01 17:21:13 +0800

    fix import

--- lib/neo4j/index.ts
+++ lib/neo4j/index.ts
@@ -9,7 +9,10 @@
  ================================================= */
 import "server-only"
 
-import { createNeo4jDataSource } from "shared/adapters/neo4j/dataSource"
+import {
+  createNeo4jDataSource,
+  type Neo4jDataSource,
+} from "@/shared/adapters/neo4j/dataSource"
 
 declare global {
   // cache across HMR
