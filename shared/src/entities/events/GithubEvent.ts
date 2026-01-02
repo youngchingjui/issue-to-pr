b6bc623d1979c4df9f6d3477b9934d8c7df01afb
Ching Jui Young <young.chingjui@gmail.com>
2026-01-02 17:20:06 +0800

    updated JSDoc formatting

--- shared/src/entities/events/GithubEvent.ts
+++ shared/src/entities/events/GithubEvent.ts
@@ -11,39 +11,39 @@
 import { z } from "zod"
 
 /**
-/ Common fields shared by all workflow events
-/ @deprecated Use Github-adapter types or webhoook-adapter types instead
-*/
+ * Common fields shared by all workflow events
+ * @deprecated Use Github-adapter types or webhoook-adapter types instead
+ */
 const BaseFields = z.object({
   id: z.string(),
   timestamp: z.date(), // ISO timestamp
 })
 
 /**
-/ @deprecated Use Github-adapter types or webhoook-adapter types instead
-*/
+ * @deprecated Use Github-adapter types or webhoook-adapter types instead
+ */
 export const IssueFetchedEventSchema = BaseFields.extend({
   type: z.literal("issue.fetched"),
   content: z.string().optional(),
 })
 
 /**
-/ @deprecated Use Github-adapter types or webhoook-adapter types instead
-*/
+ * @deprecated Use Github-adapter types or webhoook-adapter types instead
+ */
 export type IssueFetchedEvent = z.infer<typeof IssueFetchedEventSchema>
 
 /**
-/ @deprecated Use Github-adapter types or webhoook-adapter types instead
-*/
+ * @deprecated Use Github-adapter types or webhoook-adapter types instead
+ */
 export const GithubEventSchema = z.discriminatedUnion("type", [
   IssueFetchedEventSchema,
 ])
 /**
-/ @deprecated Use Github-adapter types or webhoook-adapter types instead
-*/
+ * @deprecated Use Github-adapter types or webhoook-adapter types instead
+ */
 export type GithubEvent = z.infer<typeof GithubEventSchema>
 
 /**
-/ @deprecated Use Github-adapter types or webhoook-adapter types instead
-*/
+ * @deprecated Use Github-adapter types or webhoook-adapter types instead
+ */
 export type GithubEventType = GithubEvent["type"]
